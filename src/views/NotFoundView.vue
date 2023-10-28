52cf459667e2d1f2f4ffb338982163504717c90f
ZheerBarzan <zheerbarzan85@outlook.com>
2023-10-28 13:38:18 +0300

    adding some styling to the not found page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,4 +1,18 @@
 <template>
+<center><h1>404 not found</h1></center>
+    
 
-    <h1>404 not found</h1>
+
 </template>
+
+<style scoped>
+
+h1{
+    position: absolute;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    top: 50%;
+    left: 40%;
+}
+</style>
